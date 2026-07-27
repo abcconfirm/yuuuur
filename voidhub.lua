7889da3dab2fe90c0a47cac675723675dc57468d
abcconfirm <110566119+abcconfirm@users.noreply.github.com>
2026-07-27 21:13:27 +0200

    Add new entry for 'Build a Base and Steal'

--- voidhub.lua
+++ voidhub.lua
@@ -20,6 +20,7 @@ local GAME_SCRIPTS = {
 	[135729108619936] = "08317e6d21dbd971fd5827d32d226b36",
 	[102072869879193] = "72639995ab57a880c29acecbcda93a6a", -- Anime Astral Simulator
 	[132016691802922] = "bb7b83fa7ea677e833498389b9a1d17f", -- Build a Base and Steal
+	[77458130464788] = "bb7b83fa7ea677e833498389b9a1d17f", -- Build a Base and Steal
 	[70790155462881] = "02b68a662ce9a46f089292b030f9871f", -- Zombie Turret Farm
 	[99702578544768] = "f7c39b17dc72c6d6dd2fe5bf9936979a", -- BE A FISH BAIT!
 }
